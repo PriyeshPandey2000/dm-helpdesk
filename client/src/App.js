5d7834b5278be32341831a8cd5e33d8812e7a614
PriyeshPandey2000 <priyeshpandey2000@gmail.com>
2024-02-24 15:12:07 +0530

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import ConnectFacebook from './components/ConnectFacebook';
@@ -8,12 +8,12 @@ function App() {
   return (
     <Router>
       <div>
-        <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/connect-facebook" element={<ConnectFacebook />}/>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/connect-facebook" component={ConnectFacebook} />
           {/* Add more routes for other components/pages */}
-        </Routes>
+        </Switch>
       </div>
     </Router>
   );
